dbce8d90054bf6ed77b4c9f6a49c44d1568cea1a
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-06-04 21:06:16 +0700

    xu ly lai loi thoi gian

--- app/src/main/java/com/example/countdowntimer28042021/MainActivity.java
+++ app/src/main/java/com/example/countdowntimer28042021/MainActivity.java
@@ -1,29 +1,71 @@
 package com.example.countdowntimer28042021;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.ViewSwitcher;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageSwitcher mImageSwitcher;
+    Button mBtnStart;
+    CountDownTimer mCountDownTimer;
+    int [] mArrImage = {R.drawable.hinh_1,R.drawable.hinh_2,R.drawable.hinh_3,R.drawable.hinh_4,R.drawable.hinh_5};
+    int mIndexImage = -1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CountDownTimer countDownTimer = new CountDownTimer(5100,1000) {
-            @Override
-            public void onTick(long l) {
-                Log.d("BBB",l + "");
-            }
+        mImageSwitcher = findViewById(R.id.imageSwitcher);
+        mBtnStart = findViewById(R.id.buttonStartBanner);
 
+//        mImageSwitcher.setImageResource(R.drawable.hinh_1);
+        mImageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
             @Override
-            public void onFinish() {
-
+            public View makeView() {
+                ImageView imageView = new ImageView(MainActivity.this);
+                return imageView;
             }
-        };
-        countDownTimer.start();
+        });
+
+        mBtnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCountDownTimer = new CountDownTimer(mArrImage.length * 1000, 1000) {
+                    @Override
+                    public void onTick(long l) {
+                        mIndexImage += 1;
+                        mImageSwitcher.setImageResource(mArrImage[mIndexImage]);
+                        Log.d("BBB","OnTich " + mIndexImage);
+
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        if (mIndexImage < mArrImage.length - 1){
+                            mIndexImage += 1;
+                            mImageSwitcher.setImageResource(mArrImage[mIndexImage]);
+                        }
+                        Log.d("BBB","onFinish " + mIndexImage);
+                    }
+                };
+                mCountDownTimer.start();
+            }
+        });
+
+
     }
 }
